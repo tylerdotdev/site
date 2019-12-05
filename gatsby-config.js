f3630de6d8478ed914da3588f87fbfeda047af77
Tyler Anton <mtyleranton@gmail.com>
2019-12-05 15:18:56 -0800

    styled components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `@tylerdotdev`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
